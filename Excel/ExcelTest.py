71d678d20dbcc23eaf0bc4b4634ff262459f2966
bluerain303 <yishili.sweden@yahoo.com>
2023-10-25 23:36:06 +0200

    Add Excel test

---
+++ Excel/ExcelTest.py
@@ -0,0 +1,10 @@
+from openpyxl import Workbook
+
+wb = Workbook()
+ws = wb.active
+treeData = [["Type", "Leaf Color", "Height"], ["Maple", "Red", 549], ["Oak", "Green", 783], ["Pine", "Green", 1204]]
+
+for row in treeData:
+    ws.append(row)
+
+wb.save("TreeData.xlsx")
